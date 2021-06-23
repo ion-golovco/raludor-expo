c710c3953c3190bf0746d9cb29fb88a6606b0658
ion-golovco <gaminglions2@gmail.com>
2021-06-23 11:04:07 +0300

    trey2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,42 +4,51 @@ import { Col, Row } from "reactstrap";
 import { useTranslation } from "react-i18next";
 
 export default function Contact() {
-  const {t}=useTranslation()
+  const { t } = useTranslation();
 
-    return (
-
-       <div id="contact">
-         <div id="background">
-           <Col xs="12"md={{ size: "10", offset: 1 }}>
-         <Row>
-         <Col>
-        <div
-          class="fb-page"
-          data-href="https://www.facebook.com/Proiectare-vii-livezi-106166314402118"
-          data-tabs="timeline"
-        >
-          <blockquote
-            cite="https://www.facebook.com/Proiectare-vii-livezi-106166314402118"
-            class="fb-xfbml-parse-ignore"
-          >
-            <a href="https://www.facebook.com/Proiectare-vii-livezi-106166314402118">
-              Proiectare vii livezi
-            </a>
-          </blockquote>
-        </div>
+  return (
+    <div id="contact">
+      <div id="background">
+        <Col xs="12" md={{ size: "6", offset:3 }}>
+          <Row>
+          <Col xs="3" md="3">
+            <div style={{ "margin-left": "2rem","margin-top": "2rem" }}>
+              <Row>
+                <img
+                  id="selected"
+                  src="https://img.icons8.com/ios-filled/90/f1b24a/facebook-new.png"
+                />
+                <img src="https://img.icons8.com/ios-filled/90/ffffff/facebook-new.png" />
+              </Row>
+              <Row>
+                <img
+                  id="selected"
+                  src="https://img.icons8.com/ios-filled/90/f1b24a/facebook-messenger.png"
+                />
+                <img src="https://img.icons8.com/ios-filled/90/ffffff/facebook-messenger.png" />
+              </Row>
+              <Row>
+                <img
+                  id="selected"
+                  src="https://img.icons8.com/ios-filled/90/f1b24a/whatsapp--v1.png"
+                />
+                <img src="https://img.icons8.com/ios-filled/90/ffffff/whatsapp--v1.png" />
+              </Row>
+            </div>
+            </Col>
+            <Col xs="9"md="9">
+            <div >
+              <h1>{t("contact")}</h1>
+              <h4 style={{ "margin-top": "4rem" }}>Republica Moldova</h4>
+              <h4>Chișinău, str . Tighina 49A</h4>
+              <h4>E-mail : dorin_talmaci@mail.ru</h4>
+              <h4>+373 60192045</h4>
+            </div>
+            </Col>
+            
+          </Row>
         </Col>
-        <Col>
-        <div style={{"margin":"4rem"}}>
-        <h1>{t('contact')}</h1>
-        <h4 style={{"margin-top":"4rem" }}>Republica Moldova</h4>
-        <h4>Chișinău, str . Tighina 49A</h4>
-        <h4>E-mail : dorin_talmaci@mail.ru</h4>
-        <h4>+373 60192045</h4>
-        </div>
-        </Col>
-        </Row>
-        </Col>
-        </div>
       </div>
-    )
+    </div>
+  );
 }
